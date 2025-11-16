b1ceb601fd9da1145a661ea644adee8a0de037c4
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2025-11-16 16:46:57 +0530

    bug fixes and db configs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
+import cors from "cors";
 import app from "./app";
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: ["https://task-management-mocha-mu.vercel.app/"],
+  credentials: true,
+}));
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`); 
